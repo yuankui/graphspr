ea81fa009266946c91962a59650c93668bc875c9
yuankui <yuankui@meituan.com>
2019-01-07 21:32:34 +0800

    init

---
+++ src/main/java/me/yuankui/graphspr/GraphsprApplication.java
@@ -0,0 +1,14 @@
+package me.yuankui.graphspr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphsprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphsprApplication.class, args);
+	}
+
+}
+
